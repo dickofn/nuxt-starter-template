b4b2f5921ecadd1c3397ed45ef175419c625a5f5
Dicko Finus <dickofn@gmail.com>
2023-01-13 17:50:53 +0700

    feat: add nuxt-image and google font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,18 @@
 export default defineNuxtConfig({
   modules: [
     "@nuxtjs/tailwindcss",
+    "@nuxt/image-edge",
+    "@nuxtjs/google-fonts",
   ],
   typescript: {
     shim: false,
+  },
+  googleFonts: {
+    families: {
+      Montserrat: {
+        wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+        ital: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+      },
+    },
   },
 })
