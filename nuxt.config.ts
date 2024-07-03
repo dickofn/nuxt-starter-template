821449431a1c904c023f1a723189347257009542
Dicko Finus <dickofn@gmail.com>
2024-07-03 15:37:48 +0700

    feat: added nuxt devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devtools: { enabled: true },
+
   modules: [
     "@nuxtjs/tailwindcss",
     "@nuxtjs/google-fonts",
     "@nuxtjs/eslint-module",
   ],
+
   typescript: {
     shim: false,
   },
+
   googleFonts: {
     families: {
       Montserrat: {
